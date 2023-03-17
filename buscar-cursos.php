a8f768c5c09eda4fb0dc7e0fae9846fda50b25ed
Weverton Matias <weverton-matias@hotmail.com>
2023-03-17 09:52:06 -0300

    Iniciando a requisição e coleta dos cursos do site.

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,12 +1,20 @@
 <?php
 
+require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
 
-$response = $client->request('https://www.alura.com.br/cursos-online-programacao/php');
+$response = $client->request('GET','https://www.alura.com.br/cursos-online-programacao/php');
 
 $html = $response->getBody();
 
-var_dump($html);
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach($cursos as $curso){
+    echo $curso->textContent . PHP_EOL;
+}
