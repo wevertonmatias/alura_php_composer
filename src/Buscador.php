9e00e48821bb158cdfe1943f9db819c76438f11f
Weverton Matias <weverton-matias@hotmail.com>
2023-03-23 09:52:20 -0300

    Adicionando PHPCS e ajustando para padrão PSR12.

--- src/Buscador.php
+++ src/Buscador.php
@@ -24,13 +24,13 @@ class Buscador
 
     public function buscar(string $url): array
     {
-        $response = $this->httpClient->request('GET',$url);
+        $response = $this->httpClient->request('GET', $url);
         $html = $response->getBody();
         $this->crawler->addHtmlContent($html);
         $elementosCursos = $this->crawler->filter('span.card-curso__nome');
         $cursos = [];
 
-        foreach($elementosCursos as $elemento){
+        foreach ($elementosCursos as $elemento) {
             array_push($cursos, $elemento->textContent);
         }
 
